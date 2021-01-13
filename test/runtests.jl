be987c1861752a5074a3718f78c9f4061c89baa4
sbpost <sorenpost@pm.me>
2021-01-13 22:36:29 +0100

    Changed to dynamic paths

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,16 @@ using SafeTestsets
 using DataFrames
 using Test
 
-
 # Test functions that calculate comparative advantage: ==============
-@safetestset "Comparative advantage tests" begin include("comparative-advantage-tests.jl") end
+@safetestset "Comparative advantage tests" begin include(normpath(@__DIR__, "test/comparative-advantage-tests.jl")) end
+
+# Test functions the calculate similarity and distance measures: ====
+@safetestset "Relatedness tests" begin include(normpath(@__DIR__, "test/relatedness-tests.jl")) end
 
 # Test complexity algorithms: =======================================
-@safetestset "Complexity algorithm tests" begin include("complexity-algorithm-tests.jl") end
+@safetestset "Complexity algorithm tests" begin include(normpath(@__DIR__, "test/similarity-distance-tests.jl")) end
 
 # Test forecasting functions: =======================================
+# TODO:
+
+
