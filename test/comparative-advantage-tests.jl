840a70a564e07e0c0c75489630c89c82f252708c
sbpost <sorenpost@pm.me>
2021-01-13 22:37:59 +0100

    Minor changes to format, testset separation

--- test/comparative-advantage-tests.jl
+++ test/comparative-advantage-tests.jl
@@ -1,23 +1,23 @@
 using DataFrames
 
-@testset "Basic comparative advantage tests" begin
+# Create toy data ---------------------------------
+# const COUNTRIES = ["CZE", "DEU", "HUN", "IDN"]
+# const PRODUCTS = ["1602", "1604", "1605", "1701", "1702", "1704", "1805", "1806", "1901", "1902"]
 
-	# Create toy data ---------------------------------
-	# const COUNTRIES = ["CZE", "DEU", "HUN", "IDN"]
-	# const PRODUCTS = ["1602", "1604", "1605", "1701", "1702", "1704", "1805", "1806", "1901", "1902"]
+POP = [10474410;
+       81776930;
+       10000023;
+       241834215]
 
-	POP = [10474410;
-	       81776930;
-	       10000023;
-	       241834215]
-	POP = convert(Array{Float64, 1}, POP) # Some pop-models might have "half people"
+POP = convert(Array{Float64, 1}, POP) # Some pop-models might have "half people"
 
-	EXP =  [5.66868e7 1.40287e7 1.76446e6 1.24263e8 4.21838e6 1.54845e8 3.0603e6 1.56225e8 3.14846e7 2.60488e7; # CZE
-		1.231e9 6.55645e8 7.13669e7 8.70726e8 3.60293e8 8.40892e8 2.75906e8 3.22708e9 1.16899e9 1.82894e8; # DEU
-		1.2379e8 1.00424e6 6.27152e6 1.3462e8 7.32589e7 7.9118e7 1.37126e6 7.98984e7 3.50975e7 3.30034e7; # HUN
-		5.3835e5 2.63427e8 3.20992e8 1.91463e6 1.57506e7 9.4886e7 1.10084e8 4.17582e7 5.15586e7 1.32186e8] # IDN
+EXP =  [5.66868e7 1.40287e7 1.76446e6 1.24263e8 4.21838e6 1.54845e8 3.0603e6 1.56225e8 3.14846e7 2.60488e7; # CZE
+	1.231e9 6.55645e8 7.13669e7 8.70726e8 3.60293e8 8.40892e8 2.75906e8 3.22708e9 1.16899e9 1.82894e8; # DEU
+	1.2379e8 1.00424e6 6.27152e6 1.3462e8 7.32589e7 7.9118e7 1.37126e6 7.98984e7 3.50975e7 3.30034e7; # HUN
+	5.3835e5 2.63427e8 3.20992e8 1.91463e6 1.57506e7 9.4886e7 1.10084e8 4.17582e7 5.15586e7 1.32186e8] # IDN
 
-	# Test RCA ----------------------------------------
+# Test RCA ----------------------------------------
+@testset "RCA tests" begin
 	# Check inputs:
 	@test_throws MethodError get_rca(DataFrame(EXP))
 	@test_throws MethodError get_rca(EXP[1, :])
@@ -34,8 +34,10 @@ using DataFrames
 
 	@test get_rca(EXP, binary=true)[4, 3] == 1 
 	@test get_rca(EXP, binary=true)[3, 2] == 0 
+end
 
-	# Test RpcA ---------------------------------------
+# Test RpcA ---------------------------------------
+@testset "RPCA tests" begin
 	# Check inputs:
 	@test_throws MethodError get_rpca(EXP, convert(Array{Int64, 1}, POP))
 	@test_throws MethodError get_rpca(DataFrame(EXP), POP)
